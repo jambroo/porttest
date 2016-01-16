f29ae6924b74cc73c3e72cb51887441e90b6c1b8
James Brooking <jambroo@gmail.com>
2016-01-16 14:04:34 +0100

    Testing works.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from bottle import route, run, template
+from bottle import request, route, run, template
+import socket;
+
+def test_port(ip, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  result = sock.connect_ex((ip, port))
+
+  return(result == 0)
 
 @route('/test/<port>')
 def test(port):
-    return port
+    ip = request.remote_addr
+
+    return "1" if test_port(ip, int(port)) else "0"
 
 @route('/')
 def index():
